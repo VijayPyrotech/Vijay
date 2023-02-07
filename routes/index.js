24b0a692634c7b8e9a88633405e25981706cd03d
Ragavendra <thamarayan@gmail.com>
2023-02-07 10:17:45 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -438,14 +438,13 @@ router.get('/printBillOriginal/:id', function(req,res,next){
               '--no-default-browser-check',
               '--no-first-run',
               '--no-pings',
-              '--no-sandbox',
               '--no-zygote',
               '--password-store=basic',
               '--use-gl=swiftshader',
               '--use-mock-keychain',
             ],
             executablePath: stats.executablePath,
-            headless: false
+            headless: true,
           })  
   
         
@@ -454,7 +453,7 @@ router.get('/printBillOriginal/:id', function(req,res,next){
          const page = await browser.newPage();
 
           // Configure the navigation timeout
-          await page.setDefaultNavigationTimeout(0);
+          // await page.setDefaultNavigationTimeout(0);
 
          await page.setCacheEnabled(false); 
          // set your html as the pages content
