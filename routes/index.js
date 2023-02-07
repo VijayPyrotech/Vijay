4a9e5bb30b45fb85b4219a7ed612522138bd7f9a
Ragavendra <thamarayan@gmail.com>
2023-02-07 10:31:47 +0530

    chang

--- routes/index.js
+++ routes/index.js
@@ -404,48 +404,16 @@ router.get('/printBillOriginal/:id', function(req,res,next){
       const stats = await PCR(option);
 
       // launch a new chrome instance
-          const browser = await puppeteer.launch({
-            args: [
-              '--no-sandbox',
-              '--font-render-hinting=none',
-              '--autoplay-policy=user-gesture-required',
-              '--disable-background-networking',
-              '--disable-background-timer-throttling',
-              '--disable-backgrounding-occluded-windows',
-              '--disable-breakpad',
-              '--disable-client-side-phishing-detection',
-              '--disable-component-update',
-              '--disable-default-apps',
-              '--disable-dev-shm-usage',
-              '--disable-domain-reliability',
-              '--disable-extensions',
-              '--disable-features=AudioServiceOutOfProcess',
-              '--disable-hang-monitor',
-              '--disable-ipc-flooding-protection',
-              '--disable-notifications',
-              '--disable-offer-store-unmasked-wallet-cards',
-              '--disable-popup-blocking',
-              '--disable-print-preview',
-              '--disable-prompt-on-repost',
-              '--disable-renderer-backgrounding',
-              '--disable-setuid-sandbox',
-              '--disable-speech-api',
-              '--disable-sync',
-              '--hide-scrollbars',
-              '--ignore-gpu-blacklist',
-              '--metrics-recording-only',
-              '--mute-audio',
-              '--no-default-browser-check',
-              '--no-first-run',
-              '--no-pings',
-              '--no-zygote',
-              '--password-store=basic',
-              '--use-gl=swiftshader',
-              '--use-mock-keychain',
-            ],
-            executablePath: stats.executablePath,
-            headless: true,
-          })  
+         const browser = await puppeteer.launch({
+          // ignoreDefaultArgs: ['--disable-extensions'],
+          args: [ '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-setuid-sandbox',
+        '--no-first-run',
+        '--no-sandbox',
+        '--no-zygote',
+        '--single-process',]
+        }); 
   
         
 
